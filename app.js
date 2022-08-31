fb1bf50024aae63f2e2cdf9a77a8f01eb3cf92e5
Çağın Yarkın Sarı <cyarkinsari@gmail.com>
2022-08-31 14:10:42 +0300

    connected mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Boilerplate
 const express = require("express");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,16 +15,31 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-//Posts Array
-let posts = [];
+//Database
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const postSchema = {
+  title: String,
+  content: String
+}
+
+const PostModel = mongoose.model("post", postSchema);
 
 //Home
 app.get("/", (req, res) => {
 
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  PostModel.find((err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        posts: posts
+      });
+    }
   });
+
+  
 }),
 
 //About
@@ -39,27 +55,33 @@ app.get("/contact", (req, res)=>{
 
 //Compose
 app.get("/compose", (req, res) =>{
+  
+
   res.render("compose");
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new PostModel({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
+  })
+  post.save((err)=>{
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+  
 });
 
 //express.Parameters
-app.get("/posts/:postName", (req, res) => {
+app.get("/posts/:postId", (req, res) => {
   
-  posts.forEach((post)=>{
-    if (_.lowerCase(post.title) === _.lowerCase(req.params.postName)){
-      
+  PostModel.findById(req.params.postId,(err, post)=>{
+    if (!err) {
       res.render("post", {post: post});
-    
-    };
+    }
   });
   
 });
